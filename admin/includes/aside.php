2592c7267c921af468908d586664deba733df25d
Ricard FS <alnaseos@gmail.com>
2026-06-25 13:58:20 +0200

    Modificaciones menores en el archivo "aside.php" del panel de administración.

--- admin/includes/aside.php
+++ admin/includes/aside.php
@@ -3,8 +3,8 @@
 
     <nav>
         <a href="<?= asset('/admin/dashboard.php') ?>" class="<?= $pagina === 'dashboard' ? 'active' : '' ?>"><i class="fa-notdog-duo fa-solid fa-chart-bar icon-dashboard"></i> Dashboard</a>
-        <a href="<?= asset('/admin/usuarios/usuarios.php') ?>" class="<?= $pagina === 'usuarios' ? 'active' : '' ?>"><i class="fa-solid fa-users icon-users"></i> Usuarios</a>
-        <a href="<?= asset('/admin/logs/sesiones.php') ?>" class="<?= $pagina === 'logs' ? 'active' : '' ?>"><i class="fa-solid fa-circle-user-clock icon-sessions"></i> Logs</a>
+        <a href="<?= asset('/admin/modulos/usuarios/usuarios.php') ?>" class="<?= $pagina === 'usuarios' ? 'active' : '' ?>"><i class="fa-solid fa-users icon-users"></i> Usuarios</a>
+        <a href="<?= asset('/admin/modulos/logs/sesiones.php') ?>" class="<?= $pagina === 'logs' ? 'active' : '' ?>"><i class="fa-solid fa-circle-user-clock icon-sessions"></i> Logs</a>
     </nav>
 
     <div class="logout">
