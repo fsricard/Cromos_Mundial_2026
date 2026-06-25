e0381f8a8b40e7d12ba7ccc892acdd9245091fd3
Ricard FS <alnaseos@gmail.com>
2026-06-25 12:35:56 +0200

    Añado las fuentes de "FontAwesome" al header del panel de administración.

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -12,6 +12,42 @@ if (!isset($pagina)) {
 
     <title>Admin | <?= htmlspecialchars(ucfirst($pagina)) ?></title>
 
+    <!-- FontAwesome 7.0.1 CSS -->
+    <link href="<?= asset('css/fontawesome/css/brands.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/chisel-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/duotone.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/duotone-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/duotone-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/duotone-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/etch-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/fontawesome.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/jelly-duo-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/jelly-fill-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/jelly-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/notdog-duo-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/notdog-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-duotone-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-duotone-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-duotone-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-duotone-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/sharp-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/slab-press-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/slab-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/svg.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/svg-with-js.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/thumbprint-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/v4-font-face.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/v4-shims.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/v5-font-face.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('css/fontawesome/css/whiteboard-semibold.css') ?>" rel="stylesheet" />
+
     <link rel="stylesheet" href="<?= asset('/admin/css/panel.css') ?>">
 </head>
 
