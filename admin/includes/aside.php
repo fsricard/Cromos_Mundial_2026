6d757210453cd0ec37ca5d03dfdb181268e556f1
Ricard FS <alnaseos@gmail.com>
2026-06-25 13:43:30 +0200

    Añado iconos personalizados de "FontAwesome" a los items del menú del panel de administración.

--- admin/includes/aside.php
+++ admin/includes/aside.php
@@ -2,10 +2,9 @@
     <div class="logo">Cromos 2026</div>
 
     <nav>
-        <a href="<?= asset('/admin/dashboard.php') ?>" class="<?= $pagina === 'dashboard' ? 'active' : '' ?>">Dashboard</a>
-        <a href="<?= asset('/admin/usuarios/listar.php') ?>" class="<?= $pagina === 'usuarios' ? 'active' : '' ?>">Usuarios</a>
-        <a href="<?= asset('/admin/logs/sesiones.php') ?>" class="<?= $pagina === 'logs' ? 'active' : '' ?>">Logs</a>
-        <a href="<?= asset('/admin/configuracion.php') ?>" class="<?= $pagina === 'config' ? 'active' : '' ?>">Configuración</a>
+        <a href="<?= asset('/admin/dashboard.php') ?>" class="<?= $pagina === 'dashboard' ? 'active' : '' ?>"><i class="fa-notdog-duo fa-solid fa-chart-bar icon-dashboard"></i> Dashboard</a>
+        <a href="<?= asset('/admin/usuarios/usuarios.php') ?>" class="<?= $pagina === 'usuarios' ? 'active' : '' ?>"><i class="fa-solid fa-users icon-users"></i> Usuarios</a>
+        <a href="<?= asset('/admin/logs/sesiones.php') ?>" class="<?= $pagina === 'logs' ? 'active' : '' ?>"><i class="fa-solid fa-circle-user-clock icon-sessions"></i> Logs</a>
     </nav>
 
     <div class="logout">
