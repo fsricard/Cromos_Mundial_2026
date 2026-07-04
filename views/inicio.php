8741ed6ec8862463728e8c138916b217820cb3ba
Ricard FS <alnaseos@gmail.com>
2026-07-04 23:41:02 +0200

    Añado el archivo "views/inicio.php".

---
+++ views/inicio.php
@@ -0,0 +1,18 @@
+            <main class="layout-main">
+
+                <!-- Módulo -->
+                <section class="content">
+                    <article>
+
+                        <h2 class="content-title">
+
+                        </h2>
+
+                        <div class="content-block">
+
+                        </div>
+
+                    </article>
+                </section>
+
+            </main>
