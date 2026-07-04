558185800fbc52a656d9f9be7ce7f66920f04987
Ricard FS <alnaseos@gmail.com>
2026-07-04 23:45:43 +0200

    Añado el archivo "includes/footer.php".

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+        <footer class="container-footer">
+            <div class="footer">
+                <div class="sections-footer">
+                    <div class="sections-footer-left">
+                        <h3>Esas cosillas legales</h3>
+                        <ul>
+                            <li><a href="<?= asset('/') ?>">Así es Noemí</a></li>
+                            <li><a href="<?= asset('/') ?>">Contacta con Noemí</a></li>
+                            <li><a href="<?= asset('/') ?>">Política de privacidad</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="sections-footer-center">
+                        <h3>Apoya a El Arca</h3>
+                        <ul>
+                            <li><a href="<?= asset('/') ?>">Danos tu opinión</a></li>
+                            <li><a href="<?= asset('/') ?>">Adopta un bichi</a></li>
+                            <li><a href="<?= asset('/') ?>">Apadrina un bichi</a></li>
+                            <li><a href="<?= asset('/') ?>">Colabora con El Arca</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="sections-footer-right">
+                        <img src="<?= asset('/img/cromos-mundial-2026.png') ?>" alt="Logotipo Cromos Mundial 2026" class="footer-logo" />
+                    </div>
+                </div>
+
+                <hr />
+
+                <h4>
+                    <?php
+                    echo CopyrightRicardFS();
+                    ?>
+                </h4>
+            </div>
+        </footer>
+
+        </body>
+
+        </html>
