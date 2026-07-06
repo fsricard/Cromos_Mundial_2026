5a967c8b91547d47716c59ec6bfbfe58a6c12042
Ricard FS <alnaseos@gmail.com>
2026-07-06 21:23:55 +0200

    Vambios en las tipografías de Google Fonts y en el CSS de l FrontEnd.

--- includes/header.php
+++ includes/header.php
@@ -46,7 +46,10 @@ require_once(__DIR__ . '/../config/funciones.php');
     <link href="<?= asset('/css/fontawesome/css/whiteboard-semibold.css') ?>" rel="stylesheet" />
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Russo+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="<?= asset('/css/style.css') ?>" />
 
