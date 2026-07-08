314863fc7741831202218dbf49fd8e90af92ecfd
Ricard FS <alnaseos@gmail.com>
2026-07-09 01:54:14 +0200

    Añado el archivo "views/panel/dashboard.php".

---
+++ views/panel/dashboard.php
@@ -0,0 +1,11 @@
+<section class="content">
+    <article>
+
+        <h2 class="content-title">Bienvenido al panel</h2>
+
+        <div class="content-block">
+            <p>Hola <?= htmlspecialchars($_SESSION['usuario_nombre']) ?>, este es tu panel personal.</p>
+        </div>
+
+    </article>
+</section>
