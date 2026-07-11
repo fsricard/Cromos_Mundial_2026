ffd5c6f270ccaebe4abff7c77c5aacd3eda25956
Ricard FS <alnaseos@gmail.com>
2026-07-11 03:44:53 +0200

    Añado más botones.

--- views/logout.php
+++ views/logout.php
@@ -39,6 +39,42 @@ $clase_animaciones = $ajustes['animaciones'] ? 'animaciones-on' : 'animaciones-o
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Confirmar salida</title>
+
+    <!-- FontAwesome 7.0.1 CSS -->
+    <link href="<?= asset('/css/fontawesome/css/brands.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/chisel-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/duotone.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/duotone-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/duotone-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/duotone-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/etch-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/fontawesome.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/jelly-duo-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/jelly-fill-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/jelly-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/notdog-duo-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/notdog-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-duotone-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-duotone-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-duotone-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-duotone-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/sharp-thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/slab-press-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/slab-regular.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/solid.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/svg.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/svg-with-js.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/thin.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/thumbprint-light.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/v4-font-face.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/v4-shims.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/v5-font-face.css') ?>" rel="stylesheet" />
+    <link href="<?= asset('/css/fontawesome/css/whiteboard-semibold.css') ?>" rel="stylesheet" />
     
     <link rel="stylesheet" href="<?= asset('/css/panel.css') ?>">
 </head>
@@ -53,12 +89,12 @@ $clase_animaciones = $ajustes['animaciones'] ? 'animaciones-on' : 'animaciones-o
             <p>Tu sesión actual se cerrará y deberás iniciar sesión nuevamente para acceder al panel.</p>
 
             <div class="logout-buttons">
-                <a href="<?= asset('/panel') ?>" class="btn-cancelar">
+                <a href="<?= asset('/panel') ?>" class="btn btn-cancelar">
                     <i class="fa-solid fa-circle-xmark"></i> Cancelar
                 </a>
 
-                <a href="<?= asset('/logout-confirm') ?>" class="btn-confirmar">
-                    <i class="fa-solid fa-right-from-bracket"></i> Sí, cerrar sesión
+                <a href="<?= asset('/logout-confirm') ?>" class="btn btn-danger">
+                    <i class="fa-light fa-power-off"></i> Sí, cerrar sesión
                 </a>
             </div>
         </section>
