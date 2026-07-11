fb37bdac2905cd855700de274a29233debbf6301
Ricard FS <alnaseos@gmail.com>
2026-07-11 21:23:21 +0200

    Cambios en el CSS del módulo "views/panel/ajustes.php".

--- views/panel/dashboard.php
+++ views/panel/dashboard.php
@@ -1,10 +1,14 @@
 <section class="content">
     <article>
 
-        <h2 class="content-title">Bienvenido al panel</h2>
+        <h2 class="content-title">
+            <i class="fa-regular fa-person-waving"></i> Bienvenido al panel
+        </h2>
 
         <div class="content-block">
-            <p>Hola <?= htmlspecialchars($_SESSION['usuario_nombre']) ?>, este es tu panel personal.</p>
+            <h3>
+                <i class="fa-light fa-panel-ews"></i> Hola <?= htmlspecialchars($_SESSION['usuario_nombre']) ?>, este es tu panel personal.
+            </h3>
         </div>
 
     </article>
